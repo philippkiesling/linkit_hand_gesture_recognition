620a716593b5467d8fbc5dc447bf6c8d545128a5
philippkiesling <philipp.kiesling@student.kit.edu>
2023-01-17 19:38:35 +0100

    Test gh-actions for secret url

--- submission.py
+++ submission.py
@@ -32,8 +32,17 @@ def inference(model, img):
     results = model(img)
     return results
 
+# Parse Argument evaluation url
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--evaluation_url", help="evaluation url")
+args = parser.parse_args()
+evaluation_url = args.evaluation_url
+
 if __name__ == '__main__':
     # Read in your model here
+    print(f"evaluation_url: {evaluation_url}")
+
     model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
     # Add an inference method (returns results)
