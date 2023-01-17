51a79ca8c6ce0b903349e3dae72ca69c756133cf
philippkiesling <philipp.kiesling@student.kit.edu>
2023-01-17 19:54:41 +0100

    Test gh-actions for secret url

--- secret_submission_test.py
+++ secret_submission_test.py
@@ -5,10 +5,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--evaluation_url", help="evaluation url")
 args = parser.parse_args()
 evaluation_url = args.evaluation_url
+# request evaluation url
+import requests
+import json
+import os
 
 if __name__ == '__main__':
     # Read in your model here
+
     print(f"evaluation_url: {evaluation_url}")
+    response = requests.get(evaluation_url)
+    print(response)
+    print(response.text)
     print("run rest of script")
     #model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
